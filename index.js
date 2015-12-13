4f0b93c214f174ada7c2ff03bd7376bbaaa03735
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-12-13 20:34:48 +0000

    Add lightcontrol code

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+var lightcontrol = require("lightcontrol")
 var Service, Characteristic;
 
 function LightControlAccessory(log, config) {
     this.log = log;
     this.name = config["name"];
+    this.port = config["port"];
+    this.group = parseInt(config["group"]);
+    this.plug = parseInt(config["plug"]);
+    this.state = false;
 
     this.service = new Service.Lightbulb(this.name);
+    this.controller = new lightcontrol.Controller(this.port);
+    this.ready = false;
+
+    this.controller.on("open", (function() {
+       this.ready = true;
+    }).bind(this));
 
     var onCharacteristic = this.service.getCharacteristic(Characteristic.On);
     onCharacteristic.on('get', this.getState.bind(this));
@@ -12,24 +23,28 @@ function LightControlAccessory(log, config) {
 }
 
 LightControlAccessory.prototype.getState = function(callback) {
-    this.log("Getting light control state...");
-
-    callback(null, false);
+    callback(null, this.state);
 };
 
 LightControlAccessory.prototype.setState = function(state, callback) {
-    this.log("Setting light control state to " + state);
+    if (!this.ready) {
+        callback(new Error("Not ready"));
+        return;
+    }
+
+    this.controller.set(this.group, this.plug, state);
+    this.state = state;
 
     callback(null);
 };
 
 LightControlAccessory.prototype.getServices = function() {
     return [ this.service ];
-}
+};
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
     homebridge.registerAccessory("homebridge-lightcontrol", "lightcontrol", LightControlAccessory);
-}
+};
